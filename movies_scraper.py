29171fa74c923521ad20669c42af5b46e1b92dc1
DPMods <53286132+atulyada@users.noreply.github.com>
2023-03-30 18:00:53 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "264bccbd49c930d3d08c0720149c600055c747ce"
+api_key = "32150id1bneaxnlpenaq"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://indianshortner.com/api?api={api_key}&url={i['href']}"
+            url = f"https://devuploads.com/api/account/info?key={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
