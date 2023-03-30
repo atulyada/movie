38f7a7455fc0a1043f2265eb6d13f1ece2bff85c
DPMods <53286132+atulyada@users.noreply.github.com>
2023-03-30 09:21:41 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movie-4lnn.vercel.app"
+URL = "https://movie-4lnn-git-main-atulyada.vercel.app"
 bot = Bot(TOKEN)
 
 
